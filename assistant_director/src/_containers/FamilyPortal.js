1ccee27e8d3d9aa3dbf6fc118430a4df38485080
Tyler Clark <mtclark0518@gmail.com>
2017-10-06 13:37:49 -0600

    adds checkin info

--- assistant_director/src/_containers/FamilyPortal.js
+++ assistant_director/src/_containers/FamilyPortal.js
@@ -30,10 +30,77 @@ class FamilyPortal extends Component {
         return(
             <div className="Family">
                 {this.state.family === null && (
+                    <div>
                     <FamilyCheckIn
                         family={this.state.family}
                         families={this.props.families}
                         onFamilyLogin={this.verifyFamily.bind(this)} />
+                        <ul id='family-passwords'>
+                            <li>
+                                name: 'potter', password: '10000'
+                            </li>
+                            <li>    
+                                name: 'weasley', password: '10001'
+                            </li>
+                            <li>
+                                name: 'granger', password: '10002'
+                            </li>
+                            <li>
+                                name: 'johnson', password: '10003'
+                            </li>
+                            <li>    
+                                name: 'abbot', password: '10004'
+                            </li>
+                            <li>
+                                name: 'finch-fletchley', password: '10005'
+                            </li>
+                            <li>
+                                name: 'bones', password: '10006'
+                            </li>
+                            <li>
+                                name: 'diggory', password: '10007'
+                            </li>
+                            <li>
+                                name: 'macmillian', password: '10008'
+                            </li>
+                            <li>
+                                name: 'boot', password: '10009'
+                            </li>
+                            <li>
+                                name: 'corner', password: '10010'
+                            </li>
+                            <li>
+                                name: 'patil', password: '10011'
+                            </li>    
+                            <li>
+                                name: 'chang', password: '10012'
+                            </li>
+                            <li>
+                                name: 'lovegood', password: '10013'
+                            </li>
+                            <li>
+                                name: 'ackerly', password: '10014'
+                            </li>
+                            <li>
+                                name: 'bulstrode', password: '10015'
+                            </li>
+                            <li>
+                                name: 'crabbe', password: '10016'
+                            </li>
+                            <li>
+                                name: 'goyle', password: '10017'
+                            </li>
+                            <li>
+                                name: 'malfoy', password: '10018'
+                            </li>
+                            <li>
+                                name: 'zabini', password: '10019'
+                            </li>
+                            <li>
+                                name: 'longbottom', password: '10020'
+                            </li>
+                        </ul>
+                    </div>
                 )}
                 {this.state.family !== null && (
                     <FamilyDash
